4f1d405aa82c030ded4b56f7e96ba41ca55481e9
plamenhp <p.petrov.mail@gmail.com>
2021-04-06 18:07:07 +0100

    OAuth Jwt

---
+++ 05-OAuth-JWT/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace _05_OAuth_JWT
+{
+    public static class Constants
+    {
+        public const string Issuer = "https://localhost:44398/";
+        public const string Audience = "https://localhost:44398/";
+        public const string Secret = "some_secret_ahdsfhhdf756577657lsjlsjfdl";
+    }
+}
---
+++ 05-OAuth-JWT/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using _05_OAuth_JWT;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [Authorize]
+        public IActionResult Secret() => View();
+
+        public IActionResult Authenticate()
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "some_id"),
+                new Claim("chocolate", "cookie")
+            };
+
+            var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+            var key = new SymmetricSecurityKey(secretBytes);
+            var algorithm = SecurityAlgorithms.HmacSha256;
+
+            var signingCredentials = new SigningCredentials(key, algorithm);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer,
+                Constants.Audience,
+                claims,
+                notBefore:DateTime.Now,
+                DateTime.Now.AddDays(1),
+                signingCredentials);
+
+            var jsonToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new { jwt_access_token = jsonToken});
+        }
+
+        public IActionResult Decode(string part)
+        {
+            var bytes = Convert.FromBase64String(part);
+            return Ok(Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
